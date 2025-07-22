988aadbd03606df542d756ff0ddcfe78766e8569
Esteban Ramirez N. <155033427+ERamireznu@users.noreply.github.com>
2025-07-21 22:01:24 -0400

    Update houseplan_v01.py

--- houseplan_v00.py
+++ houseplan_v00.py
@@ -1,8 +1,39 @@
 import pandas as pd       
-import turtle  
+import turtle 
+def dimens(tur0, sta_coords, col, st_dire, dimens_3, wri_coords, words):
+    #(instance, (coords start x,y), color, start_direc, (dim1, dim2, dim3),
+    #(coords write x,y),(writing, Tr/Fa, le/ri, font))
+    #standard mode
+    stx,sty = sta_coords[0],sta_coords[1]
+    dim1, dim2, dim3 = dimens_3[0],dimens_3[1],dimens_3[2]
+    wrx,wry = wri_coords[0],wri_coords[1]
+    wor0,wor1,wor2,wor3 = words[0],words[1],words[2],words[3]
+    if st_dire.lower() == 'east':  
+        tur0.setheading(0)
+    elif st_dire.lower() == 'north':  
+        tur0.setheading(90)
+    elif st_dire.lower() == 'west':  
+        tur0.setheading(180)        
+    elif st_dire.lower() == 'south':  
+        tur0.setheading(270)
+    tur0.goto(stx,sty)
+    tur0.color(col)
+    tur0.pendown()
+    tur0.forward(dim1)
+    tur0.left(90)
+    tur0.forward(dim2)
+    tur0.left(90)
+    tur0.forward(dim3)
+    tur0.penup()
+    tur0.goto(wrx,wry)
+    tur0.pendown()    
+    tur0.write(wor0,wor1,wor2,wor3)
+    tur0.penup()
+
 def rects(lis00):
+    dim_max_horiz,dim_max_vert,draw_scale = True, True, True
     calc_data = True
-    height = 2.40
+    height = 2.40  #(m)
     skk = turtle.Turtle()
     skk.speed(0)
     skk.penup()
@@ -13,8 +44,7 @@ def rects(lis00):
             skk.forward(x[2]) 
             skk.left(90)
             skk.forward(x[3])
-            skk.left(90)
-                                    
+            skk.left(90)                               
         skk.penup()
         skk.goto(x[0]+int(x[2]*0.5),x[1]+int(x[3]*0.5))
         skk.pendown()
@@ -22,96 +52,51 @@ def rects(lis00):
         skk.penup()
 
     x_low = min([x[0] for x in lis00])        
-    y_low = min([x[1] for x in lis00]) 
-
-    #draw max measures (horizontal):    
+    y_low = min([x[1] for x in lis00])   
     draw_wi = max([x[0]+x[2] for x in lis00])
     draw_he = max([x[1]+x[3] for x in lis00])
-    skk.goto(x_low, y_low-70)
-    skk.color('blue')
-    skk.pendown()
-    skk.right(90)
-    skk.forward(5*esc)
-    skk.left(90)
-    skk.forward(draw_wi*esc)
-    skk.left(90)
-    skk.forward(5*esc)
-    skk.penup()
-    skk.goto(int(draw_wi*0.5), y_low-72)
-    skk.pendown()    
-    skk.write(f"{round(draw_wi*0.01, 2)}m", False,'left',font=('Arial',7,'normal'))
-    skk.penup()
-    skk.right(90)
+    if dim_max_horiz:   #draw max measures (horizontal)
+        dimens(skk,(x_low, y_low-70),'blue','south',(5,draw_wi,5),(int(draw_wi*0.5), y_low-72),
+               (f"{round(draw_wi*0.01, 2)}m", False,'left',('Arial',7,'normal')))
+    if dim_max_vert:    #draw max measures (vertical)
+        dimens(skk,(x_low-70, y_low),'blue','east',(5,draw_he,5),(x_low-130, int(draw_he*0.5)),
+           (f"{round(draw_he*0.01, 2)}m", False,'left',('Arial',7,'normal')))    
+    if draw_scale:
+        dimens(skk,(x_low, y_low-120),'grey','south',(5,100,5),(x_low-30, y_low-135),
+           ('1m', False,'left',('Arial',7,'normal')))
     
-    #draw max measures (vertical):    
-    skk.goto(x_low-70, y_low)
-    skk.pendown()
-    skk.forward(5*esc)
-    skk.left(90)
-    skk.forward(draw_he*esc)
-    skk.left(90)
-    skk.forward(5*esc)
-    skk.penup()
-    skk.goto(x_low-130, int(draw_he*0.5))
-    skk.pendown()    
-    skk.write(f"{round(draw_he*0.01, 2)}m", False,'left',font=('Arial',7,'normal'))
-    skk.penup()
-    skk.right(180)
-    
-    #draw scale:
-    skk.goto(x_low, y_low-120)
-    skk.color('grey')
-    skk.pendown()
-    skk.right(90)
-    skk.forward(5*esc)
-    skk.left(90)
-    skk.forward(100*esc)
-    skk.left(90)
-    skk.forward(5*esc)
-    skk.penup()
-    skk.goto(x_low-30*esc, y_low-135*esc)
-    skk.pendown()    
-    skk.write('1m')
     skk.hideturtle()
-    
     if calc data:
         Res = []
         for i, x in enumerate(lis00):
-            Area = (x[2]*0.01*x[3]*0.01)*((1/esc)**2)
-            Perim = 2*(x[2]+x[3])*0.01*(1/esc)
-            SupV = Perim*height
-            Res.append((i+1, round(Area,2), round(Perim,2), round(SupV,2)))
-        
-        TotArea = sum([x[1] for x in Res])
-        TotPerim = sum([x[2] for x in Res])
-        TotSupV = sum([x[3] for x in Res])
-        Res.append(('All', TotArea, TotPerim, TotSupV))
-        df = pd.DataFrame(Res, columns = ('#','Area[m2]','Perim[mL]','SupV[m2]'))    
+            Area = round((x[2]*0.01*x[3]*0.01), 2)
+            Perim = round(2*(x[2]+x[3])*0.01, 1)
+            SupV = round(Perim*height, 2)
+            Res.append((i+1, x[2]/100, x[3]/100, Area, Perim, SupV))
+        TotArea = round(sum([x[3] for x in Res]), 2)
+        TotPerim = round(sum([x[4] for x in Res]), 1)
+        TotSupV = round(sum([x[5] for x in Res]), 2)
+        Res.append(('All', '-','-',TotArea, TotPerim, TotSupV))
+        df = pd.DataFrame(Res, columns = ('#','Side 1[m]','Side 2[m]','Area[m2]','Perim[mL]','SupV[m2]'))    
         print(df.to_string(index=False))
     turtle.done()
 
+#-----------------------------------------------------
+##subdivisions of a place: (x, y, long x, long y)
+#example: depto lazo:
+sbdvs = [(0,0,372,273),(380,0,198,138),(380,148,198,125),(292,273,80,57),(292,330,80,223),
+         (380,283,198,126),(380,419,198,129),(0,281,284,250),(0,553,680,276),(680,553,97,265),
+         (0,837,81,151),(81,837,63,93),(144,837,402,151),(546,829,134,159)]
+
+#-----------------------------------------------------
 # Create a screen object
 screen = turtle.Screen()
-
-##subdivisions of a place: (x, y, long x, long y)
-#ex.: depto lazo:
-sbdvs = [(0,0,372,273),(380,0,198,138),(380,148,198,125),(292,273,80,57),(292,330,80,215),
-         (380,283,198,126),(380,419,198,129),(0,281,284,250),(0,553,680,276),(680,553,97,265),
-         (0,837,81,151),(81,837,63,93),(144,837,402,151),(546,837,134,158)]
-global esc
-esc = 1
-sbdvs_esc = [(int(x[0]*esc),int(x[1]*esc),x[2]*esc,x[3]*esc) for x in sbdvs]        
-
-desp = [-500,-500]
-sbdvs2 = [(x[0]+desp[0],x[1]+desp[0],x[2],x[3])for x in sbdvs]
-
 #canvas size:
 cnv_wi = max([x[0]+x[2] for x in sbdvs])+300
 cnv_he = max([x[1]+x[3] for x in sbdvs])+300
 screen.screensize(cnv_wi, cnv_he)
 #set coordinates:
 screen.setworldcoordinates(-150, -150, cnv_wi, cnv_he)
-print(f"Canvas (width, height): {cnv_wi}, {cnv_he}")
 
 rects(sbdvs)
 
